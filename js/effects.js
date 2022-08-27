885010bfb74052fa765f0a52803820c65262dcb1
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2022-08-27 20:59:31 -0300

    Ajustando efeitos do menu e adicionando audio FX

--- js/effects.js
+++ js/effects.js
@@ -6,9 +6,13 @@ const styleContent1 = document.querySelector(".style-content1");
 const styleContent2 = document.querySelector(".style-content2");
 const styleContent3 = document.querySelector(".style-content3");
 
+const hover = new Audio('./media/hover.mp3');
+hover.volume = 0.03;
+
 const blur1 = () => {
    titulo1.classList.add('blur-titulos');
-   styleContent1.style.opacity = 0.8;
+   styleContent1.style.opacity = 0.9;
+   hover.play();
    titulo1.addEventListener('mouseover', blur1);
 
 }
@@ -22,7 +26,8 @@ const blur1_out = () => {
 
 const blur2 = () => {
    titulo2.classList.add('blur-titulos');
-   styleContent2.style.opacity = 0.8;
+   styleContent2.style.opacity = 0.9;
+   hover.play();
    titulo2.addEventListener('mouseover', blur2);
 
 }
@@ -36,7 +41,8 @@ const blur2_out = () => {
 
 const blur3 = () => {
    titulo3.classList.add('blur-titulos');
-   styleContent3.style.opacity = 0.8;
+   styleContent3.style.opacity = 0.9;
+   hover.play();
    titulo3.addEventListener('mouseover', blur3);
 
 }
