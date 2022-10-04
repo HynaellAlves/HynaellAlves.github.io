d5227d28a3cb56d03f1df7b320d75f5eac7b6a2b
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2022-10-04 20:49:50 -0300

    mudando conceito

---
+++ js/nav.js
@@ -0,0 +1,33 @@
+const bnt_sobre = document.querySelector('.sobre');
+const bnt_social = document.querySelector('.sociais');
+const sobre = document.querySelector('.section-1');
+const sociais = document.querySelector('.section-2');
+var active;
+
+bnt_sobre.addEventListener('click', () => {
+
+   active = 1;
+
+   if (active == 1) {
+      bnt_sobre.classList.add('active');
+      bnt_social.classList.remove('active');
+      sobre.style.display = 'flex';
+      sociais.style.display = 'none';
+
+   };
+
+});
+
+bnt_social.addEventListener('click', () => {
+
+   active = 2;
+
+   if (active == 2) {
+      bnt_social.classList.add('active');
+      bnt_sobre.classList.remove('active');
+      sobre.style.display = 'none';
+      sociais.style.display = 'flex';
+
+   };
+});
+
