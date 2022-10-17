93125c4be3bb012b3cb2127e5921b2cafdc74dcf
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2022-10-17 20:43:05 -0300

    Atualizando projeto

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,17 +1,18 @@
 const bnt_sobre = document.querySelector('.sobre');
 const bnt_social = document.querySelector('.sociais');
-const start = document.querySelector('.started');
 const sobre = document.querySelector('.section-1');
 const sociais = document.querySelector('.section-2');
+const nav = document.querySelector('.nav');
 
 const width = window.innerWidth;
 console.log(width);
 
 var active;
 
-bnt_sobre.addEventListener('click', () => {
+bnt_sobre.classList.add('active');
+sobre.style.display = 'flex';
 
-   start.style.display = 'none';
+bnt_sobre.addEventListener('click', () => {
 
    active = 1;
 
@@ -26,8 +27,6 @@ bnt_sobre.addEventListener('click', () => {
 });
 
 bnt_social.addEventListener('click', () => {
-
-   start.style.display = 'none';
 
    active = 2;
 
