619b361e4390aea329202427f1dc6db38410a1e7
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2022-10-22 16:52:47 -0300

    Atualizando projeto

--- public/js/nav.js
+++ public/js/nav.js
@@ -39,3 +39,65 @@ bnt_social.addEventListener('click', () => {
    };
 });
 
+/* --------------------------------- Theme --------------------------------------------------- */
+
+const switchButton = document.querySelector('.js-switchButton');
+const svg_image = document.querySelector('.svg-image');
+const select = document.querySelector('.select');
+const svg = document.querySelector('.svg-image img');
+
+var theme;
+
+const themeInitialized = (() => {
+
+   let theme_color = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+   theme = theme_color;
+
+   console.log(theme);
+   document.body.setAttribute('data-theme', theme);
+
+   if (theme == 'dark') {
+
+      svg.src = './img/moon-svgrepo-com.svg'
+      switchButton.style.flexDirection = 'row-reverse';
+      svg_image.style.borderRadius = '20px 0 0 20px';
+      select.style.borderRadius = '0 20px 20px 0';
+
+   } else if (theme == 'light') {
+
+      svg.src = './img/sun-svgrepo-com.svg'
+      switchButton.style.flexDirection = 'row';
+      svg_image.style.borderRadius = '0 20px 20px 0';
+      select.style.borderRadius = '20px 0 0 20px';
+
+   }
+
+});
+
+function switchTheme () {
+
+   if (theme == 'dark') {
+
+      svg.src = './img/sun-svgrepo-com.svg'
+      switchButton.style.flexDirection = 'row-reverse';
+      svg_image.style.borderRadius = '20px 0 0 20px';
+      select.style.borderRadius = '0 20px 20px 0';
+
+      theme = 'light';
+
+   } else if (theme == 'light') {
+
+      svg.src = './img/moon-svgrepo-com.svg'
+      switchButton.style.flexDirection = 'row';
+      svg_image.style.borderRadius = '0 20px 20px 0';
+      select.style.borderRadius = '20px 0 0 20px';
+
+      theme = 'dark';
+   }
+
+   document.body.setAttribute('data-theme', theme);
+
+};
+
+addEventListener('load', themeInitialized);
+switchButton.addEventListener('click', switchTheme);
